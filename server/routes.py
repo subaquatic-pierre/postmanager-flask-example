6bd5ea2490dd27a8c7aa3581e2cde39db72f8254
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-04-06 15:59:41 +0400

    Created list post view and template

--- server/routes.py
+++ server/routes.py
@@ -1,3 +1,4 @@
+from typing import List
 from flask import Blueprint
 from flask import request, render_template
 from postmanager.manager import PostManager
@@ -12,8 +13,49 @@ post_manager = PostManager(bucket_proxy, "post")
 # Show home page template
 @main.route("/")
 def home():
-    posts = post_manager.index
-    return render_template("index.html", posts=posts)
+
+    return render_template("index.html")
+
+# Posts route
+@main.route("/posts", methods=['GET', 'POST'])
+def posts():
+    posts: List = post_manager.index
+
+    post = {
+        'metaData':{
+            'id':0,
+            'title':'Coolest'
+        },
+        'content':{
+            'Header':'Coolest'
+        }
+    }
+
+    posts.append(post)
+
+
+    return render_template("posts.html", posts=posts)
+
+# Post with ID route
+@main.route("/posts/<string:id>", methods=['GET', 'PUT', 'DELETE'])
+def single_post():
+    posts: List = post_manager.index
+
+    post = {
+        'metaData':{
+            'id':0,
+            'title':'Coolest'
+        },
+        'content':{
+            'Header':'Coolest'
+        }
+    }
+
+    posts.append(post)
+
+
+    return render_template("posts.html", posts=posts)
+
 
 
 # # Show about privacy policy template
