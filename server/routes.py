254fcf0cb825d1ae67bdba1c855100e19bdb81ec
subaquatic-pierre <subaquatic.pierre@gmail.com>
2022-04-06 15:31:30 +0400

    Update hero

--- server/routes.py
+++ server/routes.py
@@ -1,13 +1,19 @@
 from flask import Blueprint
 from flask import request, render_template
+from postmanager.manager import PostManager
+from postmanager.proxy import BucketProxy
 
 main = Blueprint("main", __name__)
+
+bucket_proxy = BucketProxy("postmanager-flask-example", "/post")
+post_manager = PostManager(bucket_proxy, "post")
 
 
 # Show home page template
 @main.route("/")
 def home():
-    return render_template("index.html")
+    posts = post_manager.index
+    return render_template("index.html", posts=posts)
 
 
 # # Show about privacy policy template
